db73f5bc4985461bd3adf8e1bd8ead20cb2278bc
Prapatharshan S <102726362+PRABADHARSHAN@users.noreply.github.com>
2023-12-04 22:12:08 +0530

    Update basic.tsx
    Signed-off-by: Prapatharshan S <102726362+PRABADHARSHAN@users.noreply.github.com>

--- components/modal/demo/basic.tsx
+++ components/modal/demo/basic.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
       </Button>
       <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
         <p>Some contents...</p>
-        <p>Some contents...</p>
+        <p>Some contens...</p>
         <p>Some contents...</p>
       </Modal>
     </>
